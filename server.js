20cca7fa39a61ad31f140218a99fca07fa311c93
dagreatcode <dagreatcode@gmail.com>
2020-10-05 20:31:03 -0400

    fixing the server page with tutor

--- server.js
+++ server.js
@@ -12,3 +12,20 @@ connection.connect(function (err) {
     if (err) throw err;
     init();
 });
+
+// CREATE TABLE employees (
+//     id INT NOT NULL,
+//     first_name VARCHAR(100),
+//     last_name VARCHAR(100),
+//     role_id INT,
+//     manager_id INT,
+//     PRIMARY KEY (id)
+// );
+
+// employee:
+
+// id - INT PRIMARY KEY
+// first_name - VARCHAR(30) to hold employee first name
+// last_name - VARCHAR(30) to hold employee last name
+// role_id - INT to hold reference to role employee has
+// manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
