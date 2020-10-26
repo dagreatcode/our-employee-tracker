3814275c78d313260d74ba787871250597ea2250
dagreatcode <dagreatcode@gmail.com>
2020-10-26 16:19:42 -0400

    fixing the schema first for mysql2

--- schema.sql
+++ schema.sql
@@ -6,20 +6,20 @@ use employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE role (
     id INT NOT NULL,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary INT,
     department_id INT,
     PRIMARY KEY (id)
 );
 CREATE TABLE employee (
     id INT NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     PRIMARY KEY (id)
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Family1st",
-    database: "employees",
+    password: "password",
+    database: "employee_db",
 });
 
 connection.connect(function (err) {
