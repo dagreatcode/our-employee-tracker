e68a625d1b84ac91126d9de1ef52f249cd821b34
dagreatcode <dagreatcode@gmail.com>
2020-10-26 16:48:18 -0400

    Think im done with the schema, just need my seeds

--- schema.sql
+++ schema.sql
@@ -7,15 +7,14 @@ use employee_db;
 CREATE TABLE department (
     department_id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (department_id)
 );
 CREATE TABLE role (
     role_id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(15,3) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
-
+    PRIMARY KEY (role_id)
 );
 CREATE TABLE employee (
     employee_id INT NOT NULL,
@@ -23,13 +22,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id),
+    PRIMARY KEY (employee_id)
 );
-
--- employee:
-
--- id - INT PRIMARY KEY
--- first_name - VARCHAR(30) to hold employee first name
--- last_name - VARCHAR(30) to hold employee last name
--- role_id - INT to hold reference to role employee has
--- manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
--- seed.sql
+++ seed.sql
@@ -1,5 +1,10 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Vincent', 'Kendrick', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+values ('Vincent', 'Kendrick', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+values ('Barbara', 'Kendrick', 2, 1);
 
-INSERT INTO role (title, salary, department_id) values ('Managment', 70000, 1);
+INSERT INTO role (title, salary, department_id) 
+values ('Managment', 70000, 1);
 
-INSERT INTO department (name) values ('Management');
+INSERT INTO department (name) 
+values ('Management');
