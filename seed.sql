e87c28261dfdb6aa09333df7b776f556786b5b56
dagreatcode <dagreatcode@gmail.com>
2020-10-26 17:16:17 -0400

    my seeds look ok so far

--- seed.sql
+++ seed.sql
@@ -1,10 +1,33 @@
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 values ('Vincent', 'Kendrick', 1, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-values ('Barbara', 'Kendrick', 2, 1);
 
-INSERT INTO role (title, salary, department_id) 
-values ('Managment', 70000, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+values ('Barbara', 'Kendrick', 2, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+values ('Von', 'Kendrick', 3, 3);
+
+
 
 INSERT INTO department (name) 
 values ('Management');
+INSERT INTO department (name) 
+values ('Employee');
+
+
+INSERT INTO role (title, salary, department_id) 
+values ('Managment', 70000, 1);
+INSERT INTO role (title, salary, department_id) 
+values ('Employee', 50000, 2);
+
+UPDATE role
+SET title = Management, salary = 70000, department_id = 4
+WHERE first_name = "Vincent";
+
+UPDATE role
+SET title = Employee, salary = 50000, department_id = 5
+WHERE first_name = "Barbara";
+
+UPDATE role
+SET title = Management, salary = 70000, department_id = 6
+WHERE first_name = "Von";
