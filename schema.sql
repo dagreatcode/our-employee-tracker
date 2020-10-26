c88c689df326832a81c391c82ab6392e93345310
dagreatcode <dagreatcode@gmail.com>
2020-10-26 16:30:49 -0400

    fixing the schema alot of typos and its the tables needs work

--- schema.sql
+++ schema.sql
@@ -5,24 +5,25 @@ CREATE DATABASE employee_db;
 use employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    department_id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE role (
-    id INT NOT NULL,
+    role_id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary INT,
+    salary DECIMAL(15,3) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+
 );
 CREATE TABLE employee (
-    id INT NOT NULL,
+    employee_id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
 );
 
 -- employee:
